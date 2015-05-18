0ee52e864078fb2a769973946f4a6914fed059ab
Alistair Duggin <alistair.duggin@btinternet.com>
2015-05-18 22:22:58 +0100

    Set up Travis

--- tasks/gh-pages.js
+++ tasks/gh-pages.js
@@ -4,7 +4,12 @@ module.exports = function(grunt) {
   grunt.config(
     'gh-pages', {
       options: {
-        base: 'dist' + '<%= config.basePath  %>'
+        base: 'dist' + '<%= config.basePath  %>',
+        repo: 'https://' + process.env.GH_TOKEN + '@github.com/aduggin/accessibility-issues.git',
+        user: {
+          name: 'Alistair Duggin',
+          email: 'alistair.duggin@btinternet.com'
+        }
       },
       src: ['**']
     });
